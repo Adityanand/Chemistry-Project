1bf74be425e54db5550c80e1ed667e991687cd14
Aditya Anand <adityanand3003@gmail.com>
2019-09-04 21:49:16 +0530

    Initialization
    1st Commit

---
+++ Assets/GameLogic.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour {
+    public GameObject Cl;
+    public GameObject Na;
+    public GameObject NaCl;
+    public bool Fusion;
+    private void Start()
+    {
+        Na.GetComponent<BoxCollider>().enabled= true;
+        Cl.GetComponent<BoxCollider>().enabled = true;
+    }
+    // Use this for initialization
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Cl)
+        {
+            //Na.SetActive(false);
+            //Cl.SetActive(false);
+            //NaCl.SetActive(true);
+            Fusion = true;
+        }
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject==NaCl)
+        //Na.SetActive(true);
+        //Cl.SetActive(true);
+        //NaCl.SetActive(false);
+        //Debug.Log("hello"); 
+        Fusion = false;
+    }
+    // Update is called once per frame
+    void FixedUpdate () {
+		if(Fusion==true)
+        {
+            Na.SetActive(false);
+            Cl.SetActive(false);
+            NaCl.SetActive(true);
+        }
+        else if(Fusion==false)
+        {
+            Na.SetActive(true);
+            Cl.SetActive(true);
+            NaCl.SetActive(false);
+        }
+    }
+}
